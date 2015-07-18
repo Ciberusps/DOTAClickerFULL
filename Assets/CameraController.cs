62cb1f2b46d787617d8dad83eff990f4534fde35
Ciberus <ciberus.ps@gmail.com>
2015-07-18 22:50:10 +0300

    Мелкие изменения

---
+++ Assets/CameraController.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraController : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        float xFactor = Screen.width / 1920f;
+        float yFactor = Screen.height / 1080f;
+
+
+        Camera.main.rect = new Rect(0, 0, 1, xFactor / yFactor);
+    }
+}
