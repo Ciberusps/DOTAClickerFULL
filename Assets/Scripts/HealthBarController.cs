82dd27425991a2fc11ea6f54b2bf78bb1873d20f
Ciberus <ciberus.ps@gmail.com>
2015-08-17 23:32:13 +0300

    CleanSave

--- Assets/Scripts/HealthBarController.cs
+++ Assets/Scripts/HealthBarController.cs
@@ -4,43 +4,31 @@ using UnityEngine.UI;
 
 public class HealthBarController : MonoBehaviour
 {
-    public float minXHealthBar, maxXHealthBar;
-    public float currentXHealthBar;
-
-    EnemyHealth enemyHealth;
-    EnemyManager enemyManager;
-    RectTransform healthTransform;
-    RectTransform topBlickTransform;
-    RectTransform leftBlickTransform;
-    Text healthText;
+    private EnemyHealth _enemyHealth;
+    private EnemyManager _enemyManager;
+    public UISlider _healthSlider;
+    public UILabel _healthLabel;
 
     void Start ()
     {
         //Инициализация ХПбара
-        enemyManager = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyManager>();
-        //enemyHealth = GameObject.FindGameObjectWithTag("EnemyModel").GetComponent<EnemyHealth>();
-        healthTransform = GameObject.Find("VisualHealth").GetComponent<RectTransform>();
-        topBlickTransform = GameObject.Find("TopBlick").GetComponent<RectTransform>();
-        leftBlickTransform = GameObject.Find("LeftBlick").GetComponent<RectTransform>();
-        healthText = GameObject.Find("HealthText").GetComponent<Text>();
-        
-        //Вычисление максимальных, минимальных значений полоски ХПбара
-        maxXHealthBar = healthTransform.localPosition.x;
-        minXHealthBar = healthTransform.localPosition.x - healthTransform.rect.width;
-
-        currentXHealthBar = 0;
-        healthText.text = 0 + " / " + 0;
+        _enemyManager = GameObject.FindGameObjectWithTag("Enemy").GetComponent<EnemyManager>();
+        _healthLabel = GameObject.Find("HealthLabel").GetComponent<UILabel>();
+       // _healthSlider = gameObject.GetComponent<UISlider>();
     }
 
 	void Update ()
     {
-        if (!enemyManager.reborn && !enemyManager.enemyDead)
+        if (!_enemyManager.reborn && !_enemyManager.enemyDead)
         {
             //Инициализация префаба врага при появлении
-            enemyHealth = GameObject.FindGameObjectWithTag("EnemyModel").GetComponent<EnemyHealth>();
+            _enemyHealth = GameObject.FindGameObjectWithTag("EnemyModel").GetComponent<EnemyHealth>();
+            
+            SetCurrentValue();
+            SetCurrentEnemyHealth();
             
             //Эффект hpBar'а как в дотке блик движущийся с хп
-            if (enemyHealth.currentHealth < enemyHealth.maxHP)
+            if (_enemyHealth.currentHealth < _enemyHealth.maxHP)
             {
 
             }
@@ -49,31 +37,22 @@ public class HealthBarController : MonoBehaviour
 
             }
 
-            //Вычисление текущих координат ХПбара
-            currentXHealthBar = MapValues(enemyHealth.currentHealth, 0, enemyHealth.maxHP, minXHealthBar, maxXHealthBar);
-
-            //Текущие ХП/максХП
-            healthText.text = Mathf.RoundToInt(enemyHealth.currentHealth) + " / " + Mathf.RoundToInt(enemyHealth.maxHP);
-
-            //Локальные позиции полоски хп и бликов
-            healthTransform.localPosition = new Vector3(currentXHealthBar, 0, 5);
-            topBlickTransform.localPosition = new Vector3(currentXHealthBar, 0, 5);
-            leftBlickTransform.localPosition = new Vector3(currentXHealthBar, 0, 5);
+      
         }
         else
         {
-            healthText.text = 0 + " / " + 0;
-
-            //Локальная позиция всегда равна минимальному значению
-            healthTransform.localPosition = new Vector3(minXHealthBar, 0, 5);
-            topBlickTransform.localPosition = new Vector3(minXHealthBar, 0, 5);
-            leftBlickTransform.localPosition = new Vector3(minXHealthBar, 0, 5);
+       
         }
     }
 
-    //Вычисление координат полоски ХП
-    float MapValues(float x, float inMin, float inMax, float outMin, float outMax)
+    public void SetCurrentValue()
     {
-        return ((x - inMin) * (outMax - outMin) / (inMax - inMin) + outMin); ;
+        if (!_enemyManager.enemyDead && !_enemyManager.reborn)
+            _healthSlider.value = _enemyHealth.currentHealth/_enemyHealth.maxHP;
+    }
+
+    public void SetCurrentEnemyHealth()
+    {
+        _healthLabel.text = _enemyHealth.currentHealth + " / " + _enemyHealth.maxHP;
     }
 }
